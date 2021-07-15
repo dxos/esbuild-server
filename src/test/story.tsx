48db38281769945ea6458683ddf77206851a99b5
Dmytro Maretskyi <maretskii@gmail.com>
2021-07-15 19:09:13 +0200

    Simple storybook demo

---
+++ src/test/story.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Story1 = () => <h1>Story 1 component</h1>
+
+export const Story2 = () => <h1>Story 2 component</h1>
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { render } from 'react-dom'
 
+console.log()
 
-render(<h1>Hello world</h1>, document.getElementById('root'))
+interface StorybookProps {
+  stories: Record<string, FC>
+}
+
+const Storybook = ({ stories }: StorybookProps) => (
+  <div>
+    {Object.entries(stories).map(([name, Story]) => (
+      <div>
+        <h1>{name}</h1>
+        <div style={{ border: '1px solid black', padding: '20px' }}> 
+          <Story/>
+        </div>
+      </div>
+    ))}
+  </div>
+)
+
+function extractStories(mod: any): Record<string, FC> {
+  const res: Record<string, FC> = {}
+
+  for(const key of Object.keys(mod)) {
+    if(typeof mod[key] === 'function') {
+      res[key] = mod[key]
+    }
+  }
+
+  return res
+}
+
+render(<Storybook stories={extractStories(require('../test/story.tsx'))} />, document.getElementById('root'))
+
+
+
