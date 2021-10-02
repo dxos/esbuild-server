be838dc5933a97dc32fcb51cd348f93cd1a2ee59
Dmytro Maretskyi <maretskii@gmail.com>
2021-10-02 23:07:13 +0200

    New dev server implementatino

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { Plugin, serve } from 'esbuild'
+import { build, BuildResult, OutputFile, Plugin, serve } from 'esbuild'
 import { dirname, join, resolve } from 'path'
-import yargs from 'yargs'
+import yargs, { string } from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import glob from 'glob'
 import { promisify } from 'util'
@@ -9,6 +9,8 @@ import { RequestOptions } from 'https'
 import { loadConfig } from './load-config'
 import { sync as findPackageJson } from 'pkg-up' 
 import assert from 'assert'
+import { Trigger } from './trigger'
+import { readFile } from 'fs/promises'
 
 interface DevCommandArgv {
   stories: string[]
@@ -60,65 +62,34 @@ yargs(hideBin(process.argv))
 
       const packageRoot = getPackageRoot();
 
-      const serveResult = await serve({
-        servedir: join(packageRoot, 'src/ui/public'),
-      }, {
+      const devServer = new DevServer(argv.port, join(packageRoot, 'src/ui/public'), argv.verbose);
+      
+      build({
         entryPoints: {
           'index': 'entrypoint'
         },
+        outdir: '/',
         bundle: true,
+        watch: true,
+        write: false,
         platform: 'browser',
         format: 'iife',
         incremental: true,
         plugins: [
           createPlugin(files, packageRoot, process.cwd()),
+          devServer.createPlugin(),
           ...(config?.plugins ?? [])
         ],
         metafile: true,
       })
 
-      createProxy(serveResult.host, serveResult.port).listen(argv.port);
-      
-      console.log('🚀 Listening on http://localhost:8080')
+      devServer.listen();
+
+      console.log(`🚀 Listening on http://localhost:${argv.port}`)
     }
   )
   .demandCommand()
   .argv
-
-function createProxy(esbuildHost: string, esbuildPort: number) {
-  return http.createServer((req, res) => {
-    const options: RequestOptions = {
-      hostname: esbuildHost,
-      port: esbuildPort,
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    }
-
-    const proxyReq = http.request(options, proxyRes => {
-      if (proxyRes.statusCode === 404) {
-        const options: RequestOptions = {
-          hostname: esbuildHost,
-          port: esbuildPort,
-          path: '/',
-          method: 'GET',
-        }
-        const proxyReq = http.request(options, proxyRes => {
-          res.writeHead(proxyRes.statusCode!, proxyRes.headers)
-          proxyRes.pipe(res, { end: true })
-        })
-        proxyReq.end()
-      } else {
-        // Otherwise, forward the response from esbuild to the client
-        res.writeHead(proxyRes.statusCode!, proxyRes.headers)
-        proxyRes.pipe(res, { end: true })
-      }
-    })
-
-    // Forward the body of the request to esbuild
-    req.pipe(proxyReq, { end: true })
-  })
-}
 
 function getPackageRoot() {
   const pkg = findPackageJson({ cwd: __dirname });
@@ -126,9 +97,106 @@ function getPackageRoot() {
   return dirname(pkg);
 }
 
+interface ResolvedFile {
+  path: string
+  contents: Uint8Array
+}
+
+class DevServer {
+  private buildTrigger = new Trigger<BuildResult>();
+
+  constructor(
+    readonly port: number,
+    readonly staticDir: string,
+    readonly logRequests: boolean
+  ) {}
+
+  createPlugin(): Plugin {
+    return {
+      name: 'esapp-serve',
+      setup: ({ onStart, onEnd }) => {
+        let startTime: number = 0;
+        onStart(() => {
+          console.log(`Build started`)
+          startTime = Date.now()
+
+          this.buildTrigger.reset();
+        })
+  
+        onEnd((result) => {
+          console.log(`Build ended in ${((Date.now() - startTime) / 1000).toFixed(2)} seconds`)
+  
+          this.buildTrigger.wake(result);
+        })
+      }
+    }
+  }
+
+  private async resolveFile(url: string): Promise<ResolvedFile | undefined> {
+    const buildResult = await this.buildTrigger.wait()
+    
+    const output = buildResult.outputFiles?.find(file => file.path === url)
+    if(output) {
+      return {
+        path: output.path,
+        contents: output.contents,
+      }
+    }
+
+    try {
+      const path = join(this.staticDir, url)
+      const contents = await readFile(path)
+
+      return {
+          path,
+          contents,
+      }
+    } catch {}
+
+    if(url === '/') {
+      return this.resolveFile('index.html')
+    }
+
+    return undefined
+  }
+
+  listen() {
+    http.createServer(async (req, res) => {
+      const start = Date.now()
+
+      this.logRequests && console.log(`=> ${req.method} ${req.url} ${req.headers['accept']}`)
+
+      const respondWithFile = (file: ResolvedFile) => {
+        this.logRequests && console.log(`<= 200 ${file.path} (${file.contents.length} bytes) ${Date.now() - start}ms`)
+
+        res.writeHead(200)
+        res.write(file?.contents)
+        res.end()
+      }
+
+      const file = await this.resolveFile(req.url!)
+      if(file) {
+        respondWithFile(file)
+        return
+      }
+
+      const indexFile = await this.resolveFile('/')
+      if(indexFile) {
+        respondWithFile(indexFile)
+        return
+      }
+
+      this.logRequests && console.log(`<= 404`)
+
+      res.writeHead(404, 'Not found')
+      res.end()
+    }).listen(this.port)
+  }
+}
+
 function createPlugin(files: string[], packageRoot: string, projectRoot: string): Plugin {
   return {
-    name: 'esbuild-book',
+    name: 'esapp-book',
     setup: ({ onResolve, onLoad, onStart, onEnd }) => {
       onResolve({ filter: /^entrypoint$/ }, () => ({ namespace: 'esbuild-book', path: 'entrypoint' }))
       onLoad({ namespace: 'esbuild-book', filter: /^entrypoint$/ }, () => ({
@@ -145,18 +213,14 @@ function createPlugin(files: string[], packageRoot: string, projectRoot: string)
                   basePath: "${process.cwd()}",
                 });
               `
-      })),
+      }))
 
-      onResolve({ filter: /^react$/ }, () => ({ path: require.resolve('react', { paths: [projectRoot] }) }));
+      let reactResolved: string;
 
-      let startTime: number = 0;
+      onResolve({ filter: /^react$/ }, () => ({ path: reactResolved }));
+
       onStart(() => {
-        console.log(`Build started`)
-        startTime = Date.now()
-      })
-
-      onEnd(() => {
-        console.log(`Build ended in ${((Date.now() - startTime) / 1000).toFixed(2)} seconds`)
+        reactResolved = require.resolve('react', { paths: [projectRoot] })
       })
     }
   }
@@ -166,3 +230,4 @@ async function resolveFiles (globs: string[]): Promise<string[]> {
   const results = await Promise.all(globs.map(pattern => promisify(glob)(pattern)));
   return Array.from(new Set(results.flat(1)));
 }
+
---
+++ src/trigger.ts
@@ -0,0 +1,20 @@
+export class Trigger<T> {
+  _promise!: Promise<T>;
+  _wake!: (arg: T) => void;
+
+  constructor () {
+    this.reset();
+  }
+
+  wait (): Promise<T> {
+    return this._promise;
+  }
+
+  wake (arg: T) {
+    this._wake(arg);
+  }
+
+  reset () {
+    this._promise = new Promise((resolve) => { this._wake = resolve; })
+  }
+}
