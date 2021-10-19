7bab70925754c81fd2e01bb2ca80aa88ef687e96
Rich Burdon <rich.burdon@gmail.com>
2021-10-19 10:14:06 -0400

    Slightly less ugly :).

--- src/book/ui/Storybook.tsx
+++ src/book/ui/Storybook.tsx
@@ -8,54 +8,61 @@ export interface StorybookProps {
   stories: Stories
 }
 
-export const Storybook = ({ stories }: StorybookProps) => (
-  <BrowserRouter>
-    <GlobalStyle />
-    <Switch>
-      <Route path="/__story">
-        {Object.entries(stories).map(([file, mod]) =>
-          Object.entries(mod.stories).map(([name, Story]) => (
-            <Route exact path={`/__story/${file}/${name}`}>
-              <Story />
-            </Route>
-          ))
-        )}
-      </Route>
-      <Route>
-        <Main>
-          <Sidebar stories={stories} />
-          <Switch>
-            {Object.entries(stories).map(([file, mod]) =>
-              Object.keys(mod.stories).map((name) => (
-                <Route exact path={`/${file}/${name}`}>
-                  <StoryContainer src={`/__story/${file}/${name}`}/>
-                </Route>
-              ))
-            )}
-
-            <Redirect to="/" />
-          </Switch>
-        </Main>
-      </Route>
-    </Switch>
-  </BrowserRouter>
-)
-
-const Main = styled.div`
-  display: grid;
-  grid-template-columns: 200px 1fr;
-  width: 100vw;
-  height: 100vh;
-`
-
-const StoryContainer = styled.iframe`
-  width: 100%;
-  height: 100%;
-  border: none;
-`
+export const Storybook = ({ stories }: StorybookProps) => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/__story">
+          {Object.entries(stories).map(([file, mod]) =>
+            Object.entries(mod.stories).map(([name, Story]) => (
+              <Route key={`${file}-${name}`} exact path={`/__story/${file}/${name}`}>
+                <Story />
+              </Route>
+            ))
+          )}
+        </Route>
+        <Route>
+          <Main>
+            <Sidebar stories={stories} />
+            <Switch>
+              {Object.entries(stories).map(([file, mod]) =>
+                Object.keys(mod.stories).map((name) => (
+                  <Route exact path={`/${file}/${name}`}>
+                    <StoryContainer>
+                      <StoryFrame src={`/__story/${file}/${name}`}/>
+                    </StoryContainer>
+                  </Route>
+                ))
+              )}
+              <Redirect to="/" />
+            </Switch>
+          </Main>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0px;
   }
+`
+
+const Main = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`
+
+const StoryContainer = styled.div`
+  display: flex;
+  flex: 1;
+`
+
+const StoryFrame = styled.iframe`
+  width: 100%;
+  height: 100%;
+  border: none;
 `
