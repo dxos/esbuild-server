90004eff0d40fff7ada4a95d710b44b1cff98d75
Zarco Nontol <zarconontol@gmail.com>
2022-02-22 14:25:45 -0300

    feat: monorepo implemented (#18)
    * feat: adjust cli
    
    * implement monorepo
    
    * feat: add testing packages
    
    * feat: adjust rush publish
    
    * cleanup
    
    * Update CI
    
    * Add rush update step
    
    * adjust ci
    
    * add ts config for root
    
    * feat: add missing dependencies
    
    Co-authored-by: rzadp <roopert7@gmail.com>

---
+++ packages/esbuild-server-demos/stories/TestComponent.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { TestComponent } from '../src';
+
+export default {
+  title: 'examples/story-1'
+};
+
+export const Primary = () => (
+  <div>
+    <TestComponent label='asdas' />
+  </div>
+);
+
